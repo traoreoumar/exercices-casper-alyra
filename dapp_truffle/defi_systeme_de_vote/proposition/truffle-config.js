f52834bff155e31196eff5171a1d991f9a00d9db
Oumar Traore <oumar.traore.dev@gmail.com>
2021-06-28 15:53:36 +0200

    integration_et_livraison_continues (défi): configuration pour deploiement

--- dapp_truffle/defi_systeme_de_vote/proposition/truffle-config.js
+++ dapp_truffle/defi_systeme_de_vote/proposition/truffle-config.js
@@ -1,5 +1,11 @@
 const path = require("path");
 
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
+const MNEMONIC_PHRASE = process.env.MNEMONIC_PHRASE;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -9,7 +15,13 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*",
-    }
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(MNEMONIC_PHRASE, `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`),
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+    },
   },
   compilers: {
     solc: {
